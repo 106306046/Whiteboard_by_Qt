b9301b4fd67b83e29eb80c2744974a954c5b5f06
106306046 <timmy199922@gmail.com>
2023-07-23 10:51:42 +0800

    update video.py

--- video.py
+++ video.py
@@ -8,12 +8,19 @@ def make_hz_videos(width, height, hz, file_name):
         file_name + "_video.avi", cv2.VideoWriter_fourcc(*"DIVX"), hz * 2, frameSize
     )
 
+    """ mp4 file
+    out_put = cv2.VideoWriter(
+        file_name + "_video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), hz * 2, frameSize
+    )
+    """
+
+    black_img = cv2.resize(cv2.imread(black_img_path), frameSize)
+    img = cv2.resize(cv2.imread("img/" + file_name + ".png"), frameSize)
+
     for i in range(hz * 2):
         if i % 2:
-            black_img = cv2.imread(black_img_path)
             out_put.write(black_img)
         else:
-            img = cv2.imread("img/" + file_name + ".png")
             out_put.write(img)
 
     out_put.release()
