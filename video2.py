c3aa6cbce3cdee4c487e53c4c8940f0709199da8
106306046 <timmy199922@gmail.com>
2023-08-01 16:08:44 +0800

    new white board

---
+++ video2.py
@@ -0,0 +1,27 @@
+import cv2
+
+
+def make_hz_videos(width, height, hz, file_name):
+    black_img_path = "img/black.png"
+    frameSize = (width, height)
+    out_put = cv2.VideoWriter(
+        file_name + "_video.mp4", cv2.VideoWriter_fourcc(*"avc1"), hz * 2, frameSize
+    )
+
+    black_img = cv2.resize(cv2.imread(black_img_path), frameSize)
+    img = cv2.resize(cv2.imread(file_name + ".png"), frameSize)
+
+    for i in range(hz):
+        out_put.write(black_img)
+        out_put.write(img)
+
+    out_put.release()
+
+
+"""example
+
+from video import make_hz_videos
+
+make_hz_videos(150, 150, 60, "eraser")
+
+"""
